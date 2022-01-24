c4c645c094fbc49a7412f3f5f77339572c974205
Janio Muianga <janiodrey@gmail.com>
2022-01-24 15:34:33 +0200

    autenticação do usuario recebendo o código

---
+++ src/controllers/AuthUserController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { AuthUserService } from "../services/AuthUserService";
+
+class AuthUserController {
+  async handle(req: Request, res: Response) {
+    const { code } = req.body;
+
+    const service = new AuthUserService();
+
+    try {
+      const result = await service.execute(code);
+      return res.json(result);
+    } catch (err) {
+      return res.json(err.message);
+    }
+  }
+}
+
+export { AuthUserController };
