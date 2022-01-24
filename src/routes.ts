b4ae02265c8816ff08efa28d3ab6151bb098b412
Janio Muianga <janiodrey@gmail.com>
2022-01-24 17:09:21 +0200

    cadastro de mensagem

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 
 import { AuthUserController } from "./controllers/AuthUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/EnsureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthUserController().handle);
+
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
 
 export { router };
